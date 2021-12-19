e8f2cb4104f36a89aeb1fbcd5d6c2fc17baa608e
matsjoonas <matsjoonas@gmail.com>
2021-12-19 11:00:54 +0200

    Parse input

--- day-19/day-19-1.ts
+++ day-19/day-19-1.ts
@@ -2,6 +2,22 @@ const day191 = function day191(data: Buffer) {
   const input = data.toString().trim()
     .split('\r\n');
 
+  const scanners: number[][][] = [];
+  let scannerIndex = -1;
+  input.forEach((line, index) => {
+    if (line.includes('--- scanner')) {
+      scannerIndex++;
+    } else if (line !== '') {
+      if (!scanners[scannerIndex]){
+        scanners[scannerIndex] = [];
+      }
+      scanners[scannerIndex].push(line.split(',').map(Number))
+    }
+  });
+
+  console.log(scanners);
+
+
   return input;
 }
 
