27f2972f20efb27db12b584a4075fcaad3abd328
matsjoonas <matsjoonas@gmail.com>
2021-12-21 09:26:46 +0200

    Solve day 21 1

--- day-21/day-21-2.ts
+++ day-21/day-21-2.ts
@@ -1,8 +1,58 @@
-const day211 = function day211(data: Buffer) {
-  const input = data.toString().trim()
-    .split('\r\n');
-
-  return input;
+const rollDice = function rollDice(state = {index: 0}) {
+  state.index += 1;
+  if (state.index > 100) {
+    state.index = 1;
+  }
+  return state.index;
 }
 
-export default day211;
+const getRoundScore = function getRoundScore(roll: number, space: number) {
+  const position = roll + space;
+  if (position % 10 === 0) {
+    return 10;
+  }
+  return position % 10;
+}
+
+const day212 = function day212(data: Buffer) {
+  const input = data.toString().trim()
+    .split('\r\n').map(line => {
+      const chars = line.split('');
+      return [chars[chars.length - 2], chars[chars.length - 1]].filter(char => char !== ' ').join('');
+    }).map(Number);
+
+  let p1Position = input[0];
+  let p1Points = 0;
+  let p2Position = input[1];
+  let p2Points = 0;
+  let winner;
+  const state = {index: 0}
+  let counter = 0;
+  let result;
+
+  while (!winner) {
+    const roll = rollDice(state) + rollDice(state) + rollDice(state);
+    let roundScore;
+    if (counter % 2 === 0) {
+      roundScore = getRoundScore(roll, p1Position);
+      p1Points += roundScore;
+      p1Position = roundScore;
+    } else {
+      roundScore = getRoundScore(roll, p2Position);
+      p2Points += roundScore;
+      p2Position = roundScore;
+    }
+
+    counter++
+    if (p1Points >= 1000 || p2Points >= 1000) {
+      let loser = p1Points < p2Points ? p1Points : p2Points;
+      result = loser * counter * 3;
+      break;
+    }
+  }
+
+
+  return result;
+}
+
+export default day212;
