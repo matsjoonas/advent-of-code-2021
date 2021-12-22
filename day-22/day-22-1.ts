01a88d568722629df8661499cd8007e31ca65d3e
matsjoonas <matsjoonas@gmail.com>
2021-12-22 09:23:24 +0200

    Add day 22 1 solution

--- day-22/day-22-1.ts
+++ day-22/day-22-1.ts
@@ -1,8 +1,46 @@
 const day221 = function day221(data: Buffer) {
   const input = data.toString().trim()
-    .split('\r\n');
+    .split('\r\n')
+    .map(line => line.split(' ').map((item, index) => {
+      if (index !== 1) {
+        return item;
+      }
+      return item.split(',').map(item => item.slice(2).split('..').map(Number));
+    }));
 
-  return input;
+  const cubeRanges = input.filter(line => {
+    // @ts-ignore
+    return line[1][0][0] >= -50 && line[1][0][1] <= 50;
+  });
+
+  const onList = new Map;
+
+  cubeRanges.forEach(line => {
+    const action = line[0];
+    const cubeRange = line[1];
+
+    const xRange = cubeRange[0];
+    const yRange = cubeRange[1];
+    const zRange = cubeRange[2];
+    // @ts-ignore
+    for (let x = xRange[0]; x <= xRange[1]; x++) {
+      // @ts-ignore
+      for (let y = yRange[0]; y <= yRange[1]; y++) {
+        // @ts-ignore
+        for (let z = zRange[0]; z <= zRange[1]; z++) {
+          const key = [x, y, z].join(',');
+          if (action === 'on') {
+            onList.set(key, 'on')
+          } else {
+            onList.delete(key)
+          }
+        }
+      }
+    }
+
+  });
+
+  return onList.size;
 }
 
 export default day221;
--- day-22/day-22-2.ts
+++ day-22/day-22-2.ts
@@ -1,8 +1,43 @@
-const day222 = function day221(data: Buffer) {
+const day222 = function day222(data: Buffer) {
   const input = data.toString().trim()
-    .split('\r\n');
+    .split('\r\n')
+    .map(line => line.split(' ').map((item, index) => {
+      if (index !== 1) {
+        return item;
+      }
+      return item.split(',').map(item => item.slice(2).split('..').map(Number));
+    }));
 
-  return input;
+  const cubeRanges = [...input];
+
+  const onList = new Map;
+
+  cubeRanges.forEach(line => {
+    const action = line[0];
+    const cubeRange = line[1];
+
+    const xRange = cubeRange[0];
+    const yRange = cubeRange[1];
+    const zRange = cubeRange[2];
+    // @ts-ignore
+    for (let x = xRange[0]; x <= xRange[1]; x++) {
+      // @ts-ignore
+      for (let y = yRange[0]; y <= yRange[1]; y++) {
+        // @ts-ignore
+        for (let z = zRange[0]; z <= zRange[1]; z++) {
+          const key = [x, y, z].join(',');
+          if (action === 'on') {
+            onList.set(key, 'on')
+          } else {
+            onList.delete(key)
+          }
+        }
+      }
+    }
+
+  });
+
+  return onList.size;
 }
 
 export default day222;
